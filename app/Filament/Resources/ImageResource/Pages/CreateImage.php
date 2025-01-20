962d28402d1399fdb9e859db284d63aea5d5136b
CTAlberto <145429870+CTAlberto@users.noreply.github.com>
2025-01-20 10:00:54 +0100

    Resuelto el problema de subida de imágenes

--- app/Filament/Resources/ImageResource/Pages/CreateImage.php
+++ app/Filament/Resources/ImageResource/Pages/CreateImage.php
@@ -5,8 +5,12 @@ namespace App\Filament\Resources\ImageResource\Pages;
 use App\Filament\Resources\ImageResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateImage extends CreateRecord
 {
     protected static string $resource = ImageResource::class;
+
+
+
 }
