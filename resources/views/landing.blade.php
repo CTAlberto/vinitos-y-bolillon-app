58db6aa5cd79183aec080b219b6badec3dee63e3
Jrubio710 <jrubio_mesa@hotmail.com>
2025-02-03 10:30:42 +0100

    Pre landing active

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pre-Landing</title>
+    <style>
+
+        body, html {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: black;
+            cursor: pointer;
+        }
+        video {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body onclick="window.location.href='/welcome'">
+    <video autoplay muted playsinline>
+    <source src="{{ Vite::asset('resources/videos/landing.mp4') }}" type="video/mp4">
+        Tu navegador no soporta la reproducción de videos.
+    </video>
+</body>
+</html>
